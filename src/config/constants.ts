4b3dc19a58a12c8412679eac82fdf49e5d35f3fe
zfcstring <1263197305@qq.com>
2024-07-06 16:55:33 +0800

    create some files

---
+++ src/config/constants.ts
@@ -0,0 +1,10 @@
+/** 文本换行符 \r */
+export const BREAK_R = '\r';
+/** 文本换行符 \n */
+export const BREAK_N = '\n';
+/** 文本换行符 \r\n */
+export const BREAK_RN = '\r\n';
+/**
+ * 筛选值,表示全部
+ */
+export const __ALL__ = '__ALL__';
